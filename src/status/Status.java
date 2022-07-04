667ac5932d117d816b51e730f2b40582526055fd
rakibra02 <ibrahran@sheridancollege.ca>
2022-07-04 10:41:08 -0400

    UNO GAME

--- src/status/Status.java
+++ src/status/Status.java
@@ -7,27 +7,35 @@ package status;
 
 import java.util.Scanner;
 
-/** This class takes String input from user 
- * and calls method statusDetail to print the 
- * status of User-Rejected,Pending,Processing and Approved.
- * if the input is not given in String, 
- * program will accept and produce wrong results. 
- * change the code to use enums which avoids String input 
- * then print the status details. 
- * @author srinivsi 
+/**
+ * This class takes String input from user and calls method statusDetail to
+ * print the status of User-Rejected,Pending,Processing and Approved. if the
+ * input is not given in String, program will accept and produce wrong results.
+ * change the code to use enums which avoids String input then print the status
+ * details.
+ *
+ * @author srinivsi
  */
 public class Status {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)
-    {
-    Scanner in =new Scanner(System.in);
-    System.out.println("Enter the user status code (zero,one,two,three) in string");
-    String code = in.next();
-    StausUser t= new StausUser();
-    t.statusDetail(code); 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the user status code (zero,one,two,three) in string");
+        String code = in.next();
+
+        if (code == ("zero")) {
+            System.out.println("REJECTED");
+        } else if (code == ("one")) {
+            System.out.println("PENDING");
+        } else if (code == ("two")) {
+            System.out.println("PROCESSING");
+        } else if (code == ("three")) {
+            System.out.println("APPROVED");
+        }
+
     }
-    
+
 }
--- src/status/StausUser.java
+++ src/status/StausUser.java
@@ -7,28 +7,33 @@ package status;
 
 /**
  * MidTerm-Exam
+ *
  * @author srinivsi
  */
-public class StausUser 
-{
-   public void statusDetail(String code)
-{
-switch(code.toUpperCase())
-        {
-        case "ZERO": System.out.println("REJECTED");
-        break;
-        case "ONE": System.out.println("PENDING");
-        break;
-        case "TWO":
-        System.out.println("PROCESSING");
-        break;
-        case "THREE": 
-            System.out.println("APPROVED");
-        break;
-        default:
-         System.out.println("NOT VALID CODE");
-        break;
-        }
-}
- 
+public class StausUser {
+
+    public enum Number {
+        ZERO, ONE, TWO, THREE
+    };
+
+    private Number statusNum;
+    
+    public StausUser(Number statusNum){
+        this.statusNum = statusNum;
+    }
+
+    /**
+     * @return the statusNum
+     */
+    public Number getStatusNum() {
+        return statusNum;
+    }
+
+    /**
+     * @param statusNum the statusNum to set
+     */
+    public void setStatusNum(Number statusNum) {
+        this.statusNum = statusNum;
+    }
+    
 }
